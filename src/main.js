0ecdd4612fbe2a635f59af9a3a975b4e9a42b53b
r-avellar <renatoavellar5@gmail.com>
2022-10-18 17:49:53 -0300

    criando estrutura das flags do cartão

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardFlag(flag) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#df6f29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[flag][0])
+  ccBgColor02.setAttribute("fill", colors[flag][1])
+
+  ccLogo.setAttribute("src", `cc-${flag}.svg`)
+}
+
+setCardFlag("default")
